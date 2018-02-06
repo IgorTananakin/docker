7b4a25fa9547206200418f4fc0124826fc0f240d
Igor <tananakin.tank@yandex.ru>
2018-02-06 23:12:22 +0300

    create project

---
+++ index.php
@@ -0,0 +1,67 @@
+<html class="size">
+	<head> 
+		<title> Осень 2016</title>
+		<link  href="style1.css" type="text/css" rel="stylesheet"/>
+	</head>
+	<body> 
+		<div class="fon">
+			<div class="rectangle">
+
+			</div>
+			<div class="reklama">
+			<form>
+			<h1 class="zagolovok">Осень 2016</h1>
+			<h3 class="name">Новая коллекция обуви ОСЕНЬ-ЗИМА 2016</h3>
+			<p class="text">Ничто не вызывает у девушек столько восторга, как покупка новой
+			обуви. А если она ещё и соответствует модным тенденциям <b>осени-зимы 2016-2017</b>- в таком случае владелица обновки в прямом смысле срывает двойной куш</p>
+			<p><input type="submit" class="button"></p>
+			</form>
+			</div>
+		</div>
+	<div class="blok">
+	<p class="ocen">Осенняя коллекция</p>
+	<div class="content">
+
+	<div class="description">
+		<div class="boot1"></div>
+		<p>2 400 р/</p>
+		<p>Ботики</p>
+	</div>
+	
+	<div class="description">
+	<div class="boot2"></div>
+		<p>5 600 р/</p>
+		<p>Ботики</p>
+	</div>
+	
+	<div class="description">
+	<div class="boot3"></div>
+		<p>2 700 р/</p>
+		<p>Ботики</p>
+	</div>
+	
+	<div class="description">
+		<div class="boot4"></div>
+		<p>4 300 р/</p>
+		<p>Ботики</p>
+	</div>
+		
+	</div>
+	</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	</body>
+</html>
